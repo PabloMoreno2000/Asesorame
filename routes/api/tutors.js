f867416a09eed9fc2b412e234f0206a501c9b9da
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-31 16:13:14 -0600

    Added route to make a user tutor

--- routes/api/tutors.js
+++ routes/api/tutors.js
@@ -1,2 +1,52 @@
 const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
+router.put(
+  "/makeTutor/:id",
+  [
+    auth,
+    [
+      check("firstName", "Favor de ingresar un nombre").exists(),
+      check("firstSurname", "Favor de ingresar un primer apellido").exists(),
+      check("secondSurname", "Favor de ingresar un segundo apellido").exists(),
+      check("subjects", "Favor de ingresar un nombre").isArray(),
+      check("schedule", "Favor de ingresar un nombre").exists(),
+      check("tutoringLink", "Favor de ingresar un nombre").exists(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const {
+      firstName,
+      firstSurname,
+      secondSurname,
+      subjects,
+      schedule,
+      tutoringLink,
+    } = req.body;
+
+    const tutorInfo = {
+      name: {
+        firstName,
+        firstSurname,
+        secondSurname,
+      },
+      subjects,
+      schedule,
+      tutoringLink,
+    };
+    await User.findByIdAndUpdate(req.params.id, {
+      tutorInfo,
+      isTutor: true,
+    });
+  }
+);
